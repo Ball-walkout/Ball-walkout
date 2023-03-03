3a57cbc99622802ab70d7e8c541522d0d47c0dac
MIN-JU-CHO <ehdnf99@sookmyung.ac.kr>
2023-03-04 01:34:38 +0900

    Ball Move 구현 (rigidbody Velocity)

--- Assets/Script/ButtonMove.cs
+++ Assets/Script/ButtonMove.cs
@@ -2,32 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 유니티 모바일 터치 이동 구현 참고 링크
+// https://www.engedi.kr/unity/?q=YToxOntzOjEyOiJrZXl3b3JkX3R5cGUiO3M6MzoiYWxsIjt9&bmode=view&idx=3955143&t=board
 public class ButtonMove : MonoBehaviour
 {
-    private float leftSpeed=0f, rightSpeed=0f;
-
-    public void LeftButton()
-    {
-        leftSpeed = 1f;
-    }
-
-    public void RightButton()
-    {
-        rightSpeed = 1f;
-    }
+    private float speed = 5f;
+    private Rigidbody rig;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody>();
     }
 
+    private Vector3 velocity;
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(-leftSpeed * Time.deltaTime, 0, 0);
-        
-        transform.Translate(rightSpeed * Time.deltaTime, 0, 0);
-        
+        if (Input.touchCount > 0)
+
+        {
+            // 터치 끝났을 때
+            if (Input.GetTouch(0).phase == TouchPhase.Ended)
+
+            {
+
+                //Debug.Log("Ended - 손가락이 화면 위를 벗어나 떨어지게 된 그 순간, 터치가 끝난 상태: " + Input.GetTouch(0).position);
+                // 왼쪽 터치 시 왼쪽으로 이동
+                if (Input.GetTouch(0).position.x > (1920/2))
+
+                {
+                    velocity = new Vector3(Input.GetTouch(0).pressure, 0, 1);
+                    velocity *= speed;
+                    rig.velocity = velocity;
+
+                } else
+                // 오른쪽 터치 시 오른쪽으로 이동
+                {
+                    velocity = new Vector3(-Input.GetTouch(0).pressure, 0, 1);
+                    velocity *= speed;
+                    rig.velocity = velocity;
+                }
+
+                
+
+            }
+
+        }
     }
+    
 }
