72b332e23061dc588481ce4adc2c8daa012981ed
MIN-JU-CHO <ehdnf99@sookmyung.ac.kr>
2023-03-10 03:23:07 +0900

    오류 안걸리게 수정 + 레일 수정 + 카메라 방향 전환

--- Assets/Script/BallManager.cs
+++ Assets/Script/BallManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BallManager : MonoBehaviour
 {
+    [SerializeField] private PlayerCamera cam;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Coin")
@@ -16,6 +18,30 @@ public class BallManager : MonoBehaviour
             // 장애물 종류별 스코어 변경 필요 **
             GameManager.Instance.SetScore(10);
             Destroy(other.gameObject, 30f);
+        }
+        else if(other.tag == "Left")
+        {
+            cam.offsetX = 0f;
+            cam.offsetZ = -10f;
+            StartCoroutine(Rotate(0));
+        }
+        else if(other.tag == "Right")
+        {
+            cam.offsetX = -10f;
+            cam.offsetZ = 0f;
+            StartCoroutine(Rotate(1));
+        }
+    }
+
+    IEnumerator Rotate(int LR)
+    {
+        while(LR==0 && cam.transform.rotation.eulerAngles.y >= 0){
+            cam.transform.RotateAround(cam.transform.position, new Vector3(0f, -1f, 0f), 1f);
+            yield return null;
+        }
+        while(LR==1 && cam.transform.rotation.eulerAngles.y <= 90){
+            cam.transform.RotateAround(cam.transform.position, new Vector3(0f, 1f, 0f), 1f);
+            yield return null;
         }
     }
 }
--- Assets/Script/PlayerCamera.cs
+++ Assets/Script/PlayerCamera.cs
@@ -6,9 +6,9 @@ public class PlayerCamera : MonoBehaviour
 {
     public GameObject Target;               // 카메라가 따라다닐 타겟
 
-    public float offsetX = 0.0f;            // 카메라의 x좌표
+    public float offsetX = -10.0f;            // 카메라의 x좌표
     public float offsetY = 1.0f;           // 카메라의 y좌표
-    public float offsetZ = -10.0f;          // 카메라의 z좌표
+    public float offsetZ = 0.0f;          // 카메라의 z좌표
 
     public float CameraSpeed = 10.0f;       // 카메라의 속도
     Vector3 TargetPos;                      // 타겟의 위치
@@ -18,7 +18,7 @@ public class PlayerCamera : MonoBehaviour
     {
         // 타겟의 x, y, z 좌표에 카메라의 좌표를 더하여 카메라의 위치를 결정
         TargetPos = new Vector3(
-            offsetX,
+            Target.transform.position.x + offsetX,
             Target.transform.position.y + offsetY,
             Target.transform.position.z + offsetZ
             );
